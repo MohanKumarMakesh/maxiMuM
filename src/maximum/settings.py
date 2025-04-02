5a5d0f86c509cfefc9a4f8aa135000e5f24b9fd7
MohanKumarMakesh <x23377852@student.ncirl.ie>
2025-04-02 22:45:02 +0100

    change in appspec file

--- src/maximum/settings.py
+++ src/maximum/settings.py
@@ -141,7 +141,6 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'x23377852-maximum'  
 AWS_S3_REGION_NAME = 'eu-north-1'  # e.g. 'us-east-1'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
